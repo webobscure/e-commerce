b22f77efcf250e8797f1028acbcb9bd867f62a6c
webobscure <sparkygino@gmail.com>
2022-12-24 14:30:34 +0300

    add products to site

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const FooterBanner = () => {
+import { urlFor } from '../lib/client';
+const FooterBanner = ({ footerBanner: {discount, largeText1, largeText2, saleTime, smallText, midText, product, buttonText, image} }) => {
   return (
-    <div>FooterBanner </div>
-  )
-}
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h4>{largeText1}</h4>
+          <h4>{largeText2}</h4>
+          <p>{saleTime}</p>
 
-export default FooterBanner
+
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBanner;
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
-const HeroBanner = () => {
+import Link from 'next/link'
+import { urlFor } from '../lib/client'
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div className='hero-banner-item'>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <p >{heroBanner.midText}</p>
+        <p>{heroBanner.largeText1}</p>
+        <img 
+        src={urlFor(heroBanner.image)} 
+        alt="Headpones" 
+        className='hero-banner-image'/>
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>{heroBanner.product}</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
