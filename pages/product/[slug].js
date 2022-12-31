1074f27eab884c1d327d4cee582b4a5dc9480d27
webobscure <sparkygino@gmail.com>
2022-12-31 11:10:58 +0300

    product details section

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai'
+
 import { client, urlFor } from '../../lib/client'
 
 const ProductDetails = ({ product, products }) => {
@@ -9,22 +11,87 @@ const ProductDetails = ({ product, products }) => {
         <div className="product-detail-container">
             <div>
                 <div className="image-container">
-                    <img src={urlFor(image && image[0])}/>
+                    <img src={urlFor(image && image[0])} width={300} height={300}/>
                 </div>
+                {/*<div className="small-images-container">
+                  {image?.map((item, index) => (
+                    <img 
+                    src={urlFor(item)}
+                    className=""
+                    onMouseEnter=""/>
+                  ))}
+                </div> */}
+            </div>
+
+            <div className="product-detail-desc">
+              <h1>{name}</h1>
+              <div className="reviews">
+               <div>
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiOutlineStar />
+               </div>
+              <p>(20)</p>
+              </div>
+              <h4>Details: </h4>
+              <p>{details}</p>
+              <p className="price">$ {price}</p>
+              <div className="quantity">
+                <h3>Quantity: </h3>
+                <p className='quantity-desc'>
+                  <span className="minus" onClick=""><AiOutlineMinus /></span>
+                  <span className="num" onClick="">0</span>
+                  <span className="plus" onClick=""><AiOutlinePlus /></span>
+
+                </p>
+              </div>
+              <div className="buttons">
+                <button 
+                type='button' 
+                className="add-to-cart" 
+                onClick="">Add to cart</button>
+                <button 
+                type='button' 
+                className="buy-now" 
+                onClick="">Buy now</button>
+              </div>
             </div>
         </div>
+        <div className="maylike-products-wrapper"></div>
     </div>
   )
+}
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }`
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current
+    }
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
 }
 
 export const getStaticProps = async ({ params: { slug } }) => {
     const query = `*[_type == "product" && slug.current =='${slug}'][0]`;
     const productsQuery = '*[_type == "product"]';
 
-    const product = await client.fetch(productsQuery)
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery)
 
-    const products = await client.fetch(query);
-  
     return {
       props: {products, product}
     }
